ceb2e13d7b3497c42e3c79301cb0d4a4817bfb30
EnginAkman <usenakman@gmail.com>
2020-11-12 11:14:29 -0500

    B21 is successful testers

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //write your codes
+        System.out.println("Hello Git Last Day");
+        //B21 is excited
+        //B21 is dedicated
+        //B21 is successful testers
+
+    }
+}
